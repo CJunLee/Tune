f302d2fc933769ac582a939d710927a9acc5c641
CJun <cjlee@mail.ntut.edu.tw>
2019-04-26 13:44:06 +0000

    add DenseImageNet121 into __init__.py for importing it

--- keras-contrib/keras_contrib/applications/__init__.py
+++ keras-contrib/keras_contrib/applications/__init__.py
@@ -1,4 +1,4 @@
-from .densenet import DenseNet
+from .densenet import DenseNet, DenseNetImageNet121
 from .resnet import ResNet, ResNet18, ResNet34, ResNet50, ResNet101, ResNet152
 from .wide_resnet import WideResidualNetwork
 from .nasnet import NASNet, NASNetLarge, NASNetMobile
